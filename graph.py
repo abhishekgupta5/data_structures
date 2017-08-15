b76e9e23bec96c0e8bcad282b6df23aa60473ddd
Abhishek Gupta <abhishekmodi2011@gmail.com>
2017-08-15 21:04:53 +0530

    Shortest path b/w 2 nodes

--- graph.py
+++ graph.py
@@ -17,13 +17,15 @@ def find_path(graph, start, end, path=[]):
     if start not in graph:
         return None
     for node in graph[start]:
-        newpath = find_path(graph, node, end, path)
-        if newpath:
-            return newpath
+        if node not in path:
+            newpath = find_path(graph, node, end, path)
+            if newpath:
+                return newpath
     return None
 
 #Find path b/w node 'A' and 'D'
 print("A path from 'A' to 'D' is: ", find_path(graph, 'A', 'D'))
+print("\n")
 
 def find_all_paths(graph, start, end, path=[]):
     path = path + [start]
@@ -33,10 +35,30 @@ def find_all_paths(graph, start, end, path=[]):
         return []
     paths = []
     for node in graph[start]:
-        newpaths= find_all_paths(graph, node, end, path)
-        for newpath in newpaths:
-            paths.append(newpath)
+        if node not in path:
+            newpaths= find_all_paths(graph, node, end, path)
+            for newpath in newpaths:
+                paths.append(newpath)
     return paths
 
 #Find all paths b/w node 'A' and 'D'
 print("All paths from A and D are: ", find_all_paths(graph, 'A', 'D'))
+print("\n")
+
+#Find shortest path
+def find_shortest_path(graph, start, end, path=[]):
+    path = path + [start]
+    if start == end:
+        return path
+    if start not in graph:
+        return None
+    shortest = None
+    for node in graph[start]:
+        if node not in path:
+            newpath = find_shortest_path(graph, node, end, path)
+            if newpath:
+                if not shortest or len(newpath) < len(shortest):
+                    shortest = newpath
+    return shortest
+
+print("Shortest path b/w 'A' and 'D': ", find_shortest_path(graph, 'A', 'D'))
