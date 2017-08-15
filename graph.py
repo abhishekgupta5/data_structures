94563eebc9f15ea8c6cec4be8355a6593c49b95f
Abhishek Gupta <abhishekmodi2011@gmail.com>
2017-08-15 20:48:43 +0530

    Find all paths b/w two nodes in a graph

--- graph.py
+++ graph.py
@@ -24,3 +24,19 @@ def find_path(graph, start, end, path=[]):
 
 #Find path b/w node 'A' and 'D'
 print("A path from 'A' to 'D' is: ", find_path(graph, 'A', 'D'))
+
+def find_all_paths(graph, start, end, path=[]):
+    path = path + [start]
+    if start == end:
+        return [path]
+    if start not in graph:
+        return []
+    paths = []
+    for node in graph[start]:
+        newpaths= find_all_paths(graph, node, end, path)
+        for newpath in newpaths:
+            paths.append(newpath)
+    return paths
+
+#Find all paths b/w node 'A' and 'D'
+print("All paths from A and D are: ", find_all_paths(graph, 'A', 'D'))
